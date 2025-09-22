49bd1206a81d78c403ee3863a7d1c7f68484c4ee
yyyFG <2523766521@qq.com>
2025-09-23 00:40:41 +0800

    first commit

---
+++ src/main/java/cn/y/yaicodezgen/YAiCodeZgenApplication.java
@@ -0,0 +1,13 @@
+package cn.y.yaicodezgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YAiCodeZgenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YAiCodeZgenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/y/yaicodezgen/YAiCodeZgenApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.y.yaicodezgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YAiCodeZgenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
