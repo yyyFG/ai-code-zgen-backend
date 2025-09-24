10f6e82389e16905f14776808b44a65664530561
yyyFG <2523766521@qq.com>
2025-09-24 11:51:27 +0800

    add 后端项目初始化

---
+++ src/main/java/cn/y/yaicodezgen/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package cn.y.yaicodezgen.exception;
+
+import cn.y.yaicodezgen.common.BaseResponse;
+import cn.y.yaicodezgen.common.ResultUtils;
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Hidden
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e){
+        log.error("BusinessException: " + e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e){
+        log.error("RuntimeException: " + e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
