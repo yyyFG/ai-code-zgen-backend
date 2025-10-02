24dfc1f0f5e1a802daa8c5d524adcbafa2a6405b
yyyFG <2523766521@qq.com>
2025-10-02 23:09:54 +0800

    完成权限校验AOP

---
+++ src/main/java/cn/y/yaicodezgen/annotation/AuthCheck.java
@@ -0,0 +1,23 @@
+package cn.y.yaicodezgen.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 权限校验
+ *
+ * @author <a href="https://github.com/yaicodezgen">yaicodezgen</a>
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthCheck {
+
+    /**
+     * 必须有某个角色
+     *
+     * @return
+     */
+    String mustRole() default "";
+}
